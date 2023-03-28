4263a361b6c43db07ee8d583afb709ce2b420bf9
esquared13 <erin_enzinger@yahoo.com>
2023-03-28 10:18:11 -0400

    added comments

--- Program.cs
+++ Program.cs
@@ -8,19 +8,20 @@ namespace fibonaccifinder
         public static void Main()
         {
             Console.Write("Enter the desired length of the Fibonacci Sequence...");
-            ulong n = Convert.ToUInt64(Console.ReadLine()); //sets length of sequence, represented by the variable n, to user input
+            ulong n = Convert.ToUInt64(Console.ReadLine()); // sets length of sequence, represented by the variable n, to user input
 
             ulong f = 1; 
             ulong q = 0;
             ulong s = 0;
 
-            for (ulong i = 0; i <= n;)
+            for (ulong i = 0; i <= n;) // the variable i keeps track of how many calculations have been made
             {
+                // in this loop the values of variables are cycled through, since not all three values are needed at the same time
                 q = f + s;
                 i++;
                 Console.WriteLine(q);
                 
-                if (i >= n){break;}
+                if (i >= n){break;} // makes sure the sequence stops where the user specifies
                 
                 else
                 {
@@ -29,7 +30,7 @@ namespace fibonaccifinder
                     Console.WriteLine(s);
                 }
                 
-                if (i >= n){break;}
+                if (i >= n){break;} // makes sure the sequence stops where the user specifies
                 
                 else
                 {
